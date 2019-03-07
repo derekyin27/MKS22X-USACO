b7b384a4c92f36b11121860c738cd4760709d776
Derek <dyin00@stuy.edu>
2019-03-07 11:47:25 -0500

    feed into array and toString

--- USACO.java
+++ USACO.java
@@ -6,6 +6,7 @@ public class USACO{
   private int C;
   private int E;
   private int N;
+  private int[][] field;
   public static int bronze(String filename){
 return 1;
   }
@@ -21,14 +22,31 @@ public USACO(String filename){
   C = inf.nextInt();
   E = inf.nextInt();
   N = inf.nextInt();
+  field = new int[R][C];
+for (int r = 0; r < 4; r++){
+  for (int c = 0; c < 6; c++){
+field[r][c] = inf.nextInt();
+  }
+
+}
 
 }
 catch (FileNotFoundException e){};
 }
 
+public String toString(){
+  String str = "";
+  for (int r =0; r < R; r++){
+    for (int c = 0; c < C; c++){
+      str+= field[r][c]+ " ";
+    }
+    str+="\n";
+  }
+  return str;
+}
 public static void main(String[] args) {
   USACO test = new USACO("makelake.in");
-
+System.out.println(test);
 
 }
 }
